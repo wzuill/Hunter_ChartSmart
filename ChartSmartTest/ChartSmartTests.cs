c9f2c777924ed717ce411eeaa6aa0b908db440b1
Woody Zuill <wzuill@yahoo.com>
2018-04-04 12:17:45 -0700

    Added all files.... again

---
+++ ChartSmartTest/ChartSmartTests.cs
@@ -0,0 +1,74 @@
+﻿using ApprovalTests.Reporters;
+using ApprovalTests.WinForms;
+using ChartSmart;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChartSmartTest
+{
+    [TestClass]
+    public class ChartSmartTests
+    {
+        [TestMethod]
+        public void ItsAlive()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        [UseReporter(typeof(DiffReporter))]
+        public void TestThatApprovalsIsWorking()
+        {
+            ApprovalTests.Approvals.Verify("Test");
+        }
+
+        [TestMethod]
+        [UseReporter(typeof(TortoiseImageDiffReporter), typeof(ClipboardReporter))]
+        public void TestMainForm()
+        {
+            frmMain main = new frmMain();
+            main.Show();
+            ApprovalTests.WinForms.WinFormsApprovals.Verify(main);
+        }
+
+        [TestMethod]
+        [UseReporter(typeof(TortoiseImageDiffReporter), typeof(ClipboardReporter))]
+        public void TestBarChartSingle()
+        {
+            frmMain main = new frmMain();
+            ChartSingleCompareOrig chart = main.LaunchChart(406, "rpfll", false);
+            chart.Show();
+            WinFormsApprovals.Verify(chart);
+        }
+
+        [TestMethod]
+        [UseReporter(typeof(TortoiseImageDiffReporter), typeof(ClipboardReporter))]
+        public void TestBarChartCompare()
+        {
+            frmMain main = new frmMain();
+            ChartSingleCompareOrig chart = main.LaunchChart(406, "splitdisplay", false);
+            chart.Show();
+            WinFormsApprovals.Verify(chart);
+        }
+
+
+        [TestMethod]
+        [UseReporter(typeof(DiffReporter))]
+        public void TestPieChartWindow()
+        {
+            frmMain main = new frmMain();
+            ChartSingleCompareOrig chart = main.LaunchChart(712, "rpfll", false);
+            chart.Show();
+            WinFormsApprovals.Verify(chart);
+        }
+
+        [TestMethod]
+        [UseReporter(typeof(DiffReporter))]
+        public void TestPieChartCompareWindow()
+        {
+            frmMain main = new frmMain();
+            ChartSingleCompareOrig chart = main.LaunchChart(712, "splitdisplay", false);
+            chart.Show();
+            WinFormsApprovals.Verify(chart);
+        }
+    }
+}
